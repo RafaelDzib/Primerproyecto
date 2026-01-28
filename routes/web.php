9b24e6f987e9080109ba5eade7ae4628f52ff816
Rafael Dzib Alcalá <rafaey1995@gmail.com>
2026-01-27 20:01:44 -0600

    Se agrego una funcion mas en el archivo web .php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PostController;
 
 Route::get('/Hello',HomeController::class);
 Route::get('post/mensaje',[PostController::class,'Mensaje']);
+route::get('post/about/{param?}/{name?}',[PostController::class, 'about']);
 /*Route::get('/', function () {
     return view('welcome');
 });*/
